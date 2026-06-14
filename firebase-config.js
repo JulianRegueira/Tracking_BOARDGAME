d9243973d0456608093cf19f1990ec1c4fe966e4
Julian Regueira <julian_regueira@hotmail.com>
2026-06-14 12:42:49 -0300

    Fixing

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,11 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyC-9PFZlBOuNpY-_a7pXL6mMbM1R0jZ9Q8",
   authDomain: "tabletop-a3a19.firebaseapp.com",
-  databaseURL:"https://tabletop-a3a19-default-rtdb.firebaseio.com",
+  databaseURL: "https://tabletop-a3a19-default-rtdb.firebaseio.com",
   projectId: "tabletop-a3a19",
   storageBucket: "tabletop-a3a19.firebasestorage.app",
   messagingSenderId: "225167118637",
   appId: "1:225167118637:web:24da3905b1977e7bae03a4",
-  measurementId: "G-HL57QFD9NT"
 };
 
 export function isFirebaseConfigured() {
@@ -15,8 +14,11 @@ export function isFirebaseConfigured() {
     firebaseConfig.authDomain &&
     firebaseConfig.databaseURL &&
     firebaseConfig.projectId &&
+    firebaseConfig.storageBucket &&
+    firebaseConfig.messagingSenderId &&
     firebaseConfig.appId &&
     !firebaseConfig.apiKey.includes("AIzaSyC-9PFZlBOuNpY-_a7pXL6mMbM1R0jZ9Q8") &&
+    !firebaseConfig.databaseURL.includes("tabletop-a3a19") &&
     !firebaseConfig.databaseURL.includes("https://tabletop-a3a19-default-rtdb.firebaseio.com")
   );
 }
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "comandantes-v5-sync-v5.5";
+const CACHE_NAME = "comandantes-v5-sync-v5.5.1";
 
 const ASSETS = [
   "./",
